93db79a97aefe534add73468833428fc71937eee
N1ger69 <114093327+N1ger69@users.noreply.github.com>
2022-09-21 18:45:11 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+using namespace std;
+int main() {
+	/*
+	//boolean1
+	int a;
+	cin >> a;
+	bool res = a > 0;
+	cout << boolalpha << res << endl;
+	*/
+	/*
+	//boolean2
+	int a;
+	cin >> a;
+	bool res = a % 2 != 0;
+	cout << boolalpha << res << endl;
+	*/
+	/*
+	//boolean3
+	int a;
+	cin >> a;
+	bool res = a % 2 == 0;
+	cout << boolalpha << res << endl;
+	*/
+	/*
+	//boolean 4
+	int a, b;
+	cin >> a >> b;
+	bool res = a > 2 && b <= 3;
+	cout << boolalpha << res << endl;
+	*/
+	/*
+	//boolean5
+	int a, b;
+	cin >> a >> b;
+	bool res a >= 0 || b < -2;
+	cout << boolalpha << res << endl; 
+	//boolean6
+	int a, b, c;
+	cin >> a >> b >> c;
+	bool res = a < b && b < c;
+	cout << boolalpha << res << endl;
+	*/
+	/*
+	//boolean7
+	int a, b, c;
+	cin >> a >> b >> c;
+	bool res = a < b < c;
+	cout << boolalpha << res << endl;
+	*/
+	/*
+	//boolean8
+	int a, b;
+	cin >> a >> b;
+	bool res = a % 2 != 0 && b % 2 != 0;
+	cout << boolalpha << res << endl;
+	*/
+	/*
+	//boolean9
+	int a, b;
+	cin >> a >> b;
+	bool res = a % 2 == 0 && b % 2 != 0 || a % 2 != 0 && b % 2 == 0;
+	cout << boolalpha << res << endl;
+	*/
+	/*
+	//boolean10
+	int a, b;
+	cin >> a >> b;
+	bool res = a % 2 == 0 && b % 2 != 0;
+	cout << boolalpha << res << endl;
+	*/
+	/*
+	//boolean11
+	int a, b;
+	cin >> a >> b;
+	bool res = a % 2 == 0 && b % 2 == 0;
+	cout << boolalpha << res << endl;
+	*/
+	/*
+	//boolean12
+	int a, b, c;
+	cin >> a >> b >> c;
+	bool res = a > 0 && b > 0 && c > 0;
+	cout << boolalpha << res << endl;
+	*/
+	/*
+	//boolean13
+	int a, b, c;
+	cin >> a >> b >> c;
+	bool res = a > 0 && b < 0 && c < 0 || a < 0 && b > 0 && c < 0 || a < 0 && b < 0 && c > 0;
+	cout << boolalpha << res << endl;
+	*/
+	/*
+	//boolean14
+	int a, b, c;
+	cin >> a >> b >> c;
+	bool res = a < 0 && b < 0 && c > 0;
+	cout << boolalpha << res << endl;
+	*/
+	/*
+	//boolean15
+	int a, b, c;
+	cin >> a >> b >> c;
+	bool res = a > 0 && b > 0 && c < 0;
+	cout << boolalpha << res << endl;
+	*/
+	//boolean16
+/*
+int a;
+cin >> a;
+bool res = a >= 10 && a % 2 == 0;
+cout << boolalpha << res << endl;
+*/
+//boolean17
+int a;
+cin >> a;
+bool res = a >= 100 && a % 2 != 0;
+cout << boolalpha << res << endl;
+}
